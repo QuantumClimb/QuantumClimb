1b7a4791335fe7cf4e79a5267c0e867420ab9122
QuantumClimb <QuantumClimb@users.noreply.github.com>
2025-06-30 08:56:15 +0530

    Update YouTube video link to new Shorts video

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -5,7 +5,7 @@ const VideoSection = () => {
     <section className="video-section">
       <div className="video-wrapper">
         <iframe
-          src="https://www.youtube.com/embed/6A_wAEr7nLs?loop=1&mute=1&playlist=6A_wAEr7nLs&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1"
+          src="https://www.youtube.com/embed/ZXAHAMj_A1Y?loop=1&mute=1&playlist=ZXAHAMj_A1Y&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1"
           title="Quantum Climb Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
